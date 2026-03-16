b7c7959428a7cc21f1b5da35cd72116ab9d92e2d
DavidFranco135 <davidbhmg147@gmail.com>
2026-03-16 19:14:18 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDx2p5tkMaeAjL6WrHNxFvK7rKZ77iFZ4w",
-  authDomain: "financeiro-a7116.firebaseapp.com",
-  projectId: "financeiro-a7116",
-  storageBucket: "financeiro-a7116.firebasestorage.app",
-  messagingSenderId: "952138197288",
-  appId: "1:952138197288:web:11b60ce4a514d346ce081d"
+  apiKey: "AIzaSyCbe9GJygUvifFY6MR8N8m-sJh8ceGWOLU",
+  authDomain: "barbearia-premium-demo.firebaseapp.com",
+  projectId: "barbearia-premium-demo",
+  storageBucket: "barbearia-premium-demo.firebasestorage.app",
+  messagingSenderId: "972303217678",
+  appId: "1:972303217678:web:c3204ee2201bfed1e6785f"
 };
-
 // Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
 
